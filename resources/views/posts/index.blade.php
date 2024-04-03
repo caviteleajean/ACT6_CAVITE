731498f4010dfb2118b99e6e4b75fd4bc23540b9
liliqtie <joycabarlesbagalihog@gmail.com>
2024-04-03 14:58:09 +0800

    Add posts

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Activity 6 CAVITE</title>
+</head>
+<body>
+    <h1>BSIT 3C</h1>
+    <h2>My Activity 6</h2>
+    <ul>
+        <li><a href="/posts/1">POST 1</a></li>
+        <li><a href="/posts/2">POST 2</a></li>
+    </ul>
+</body>
+</html>
